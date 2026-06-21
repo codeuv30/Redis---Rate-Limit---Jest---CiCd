4141e71587f89acd77c327229c1dea05eed3aab3
codeuv30 <codeuv30@gmail.com>
2026-06-21 14:09:10 +0530

    feat: enforce required fields for user schema

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    name: String,
-    email: String,
+    name: { type: String, required: true },
+    email: { type: String, required: true },
 });
 
 const User = mongoose.model("users", userSchema);
